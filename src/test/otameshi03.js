994f1b7131fddeee75973781eef538cd7ac80829
史彦 吉田 <sakura559@gmail.com>
2018-10-12 18:47:07 +0900

    スライダーの操作を可能に

--- src/test/otameshi03.js
+++ src/test/otameshi03.js
@@ -3,6 +3,7 @@
 var starttime;
 var endtime;
 var workmsec;
+var mousey;
 
 function StartButton(){
 	starttime = Date.now();
@@ -15,3 +16,31 @@ function CloseButton() {
     var writeplace = document.getElementsByClassName("hidden");
     writeplace[0].innerHTML += workmsec.toString();
 }   
+
+
+function CatchTsumami(event) {
+    var target = document.getElementById("tsumamiimg");
+    var event2 = event || window.event;
+    WriteOutput(event2.pageY);
+
+    var yplace = event2.pageY;
+    if (yplace > 550) {
+        target.style.top = 300;
+    }
+    else if (yplace < 250) {
+        target.style.top = 0;
+    }
+    else {
+        target.style.top = yplace - 250;
+    }
+}
+
+function handleMouseMove(event) {
+    event2 = event || window.event; // IE‘Î‰ž
+    WriteOutput(event2.clientY);
+}
+
+function WriteOutput(str) {
+    var target = document.getElementById("output");
+    target.innerHTML = str;
+}
