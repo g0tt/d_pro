153c39f2d434b86ddc0a14987d16101d0dde3fdd
史彦 吉田 <sakura559@gmail.com>
2018-10-12 22:20:45 +0900

    スライダーの機能の有効化

--- src/test/otameshi03.js
+++ src/test/otameshi03.js
@@ -14,51 +14,50 @@ function CloseButton() {
     endtime = Date.now();
     workmsec = endtime - starttime;
     var writeplace = document.getElementsByClassName("hidden");
-    writeplace[0].innerHTML += workmsec.toString();
+    WriteOutput(workmsec.toString());
 }   
 
 
 function CatchTsumami(event) {
     var event2 = event || window.event;
-
     var yplace = event2.pageY;
-    WriteOutput(yplace);
-
-    if (yplace > 550) {
-        event2.target.style.top = 300 + "px";
-    }
-    else if (yplace < 250) {
-        event2.target.style.top = 0 + "px";
-    }
-    else {
-        event2.target.style.top = (yplace - 250) + "px";
-    }
+    MoveTsumami(yplace, event2.target);
 }
 
 function CatchTsumamiS(event) {
     var event2 = event.targetTouches[0];
-    var yplace = event.targetTouches[0].pageY;
-    WriteOutput(yplace);
+    var yplace = event2.pageY;
+    MoveTsumami(yplace, event2.target);
+}
 
-    if (yplace > 550) {
-        event2.target.style.top = 300 + "px";
+function MoveTsumami(yplace, targ) {
+    WriteOutput(yplace);
+    var ypos;
+    if (yplace > 600) {
+        ypos = 300;
     }
-    else if (yplace < 250) {
-        event2.target.style.top = 0 + "px";
+    else if (yplace < 300) {
+        ypos = 0;
     }
     else {
-        event2.target.style.top = (yplace - 250) + "px";
+        ypos = yplace - 300;
     }
+    targ.style.top = ypos + "px";
+    ChangeBrightness(ypos, 0);
 }
 
 
 document.addEventListener('DOMContentLoaded', function () {
     var tsumami = document.getElementById("tsumamiimg");
+    var close1 = document.getElementById("close1");
+    var close2 = document.getElementById("close2");
+
     tsumami.ondrag = CatchTsumami;
     tsumami.ondragend = CatchTsumami;
     tsumami.ontouchmove = CatchTsumamiS;
     tsumami.ontouchend = CatchTsumamiS;
-
+    close1.onclick = CloseButton;
+    close2.onclick = CloseButton;
 
 }, false);
 
@@ -71,4 +70,10 @@ function handleMouseMove(event) {
 function WriteOutput(str) {
     var target = document.getElementById("output");
     target.innerHTML = str;
+}
+
+function ChangeBrightness(ypos, changeindex) {
+    var target = document.getElementsByClassName("deslig")[changeindex];
+    var colornum = 255 - ypos / 2;
+    target.style.backgroundColor = "rgb(" + colornum + ", " + colornum + ", " + colornum + ")";
 }
