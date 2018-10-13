94e590f04240751668f8027a309d8f8894b3eb97
史彦 吉田 <sakura559@gmail.com>
2018-10-13 18:30:26 +0900

    IE対応・CSSオブジェクト化・ソフトコーディングに

--- src/test/otameshi03.js
+++ src/test/otameshi03.js
@@ -18,8 +18,6 @@ function StartButton(){
 function CloseButton() {
     endtime = Date.now();
     workmsec = endtime - starttime;
-    var writeplace = document.getElementsByClassName("hidden");
-    WriteOutput(workmsec.toString());
     ClearMission(document.getElementsByClassName("deslig")[0], workmsec);
 }   
 
@@ -34,36 +32,33 @@ var catchypos;
 var tsumamiiniypos;
 
 /* PCでつまみを動かしているとき */
-function CatchTsumami(event) {
+function CatchTsumami(event, changeind) {
     var event2 = event || window.event;
     var yplace = event2.pageY;
-    WriteOutput(yplace);
-    MoveTsumami(yplace, event2.target);
+    MoveTsumami(yplace, event2.target, changeind);
 }
 
 /* PCでつまみを掴んだ直後 */
 function CatchTsumamiSta(event) {
     catchypos = (event || window.event).pageY;
     tsumamiiniypos = Math.max(parseInt(document.getElementById("tsumamiimg").style.top) || 0, 0);
-    WriteOutput(tsumamiiniypos);
 }
 
 
 /* スマホでつまみを動かしているとき */
-function CatchTsumamiS(event) {
+function CatchTsumamiS(event, changeind) {
     var event2 = event.targetTouches[0];
     var yplace = event2.pageY;
-    MoveTsumamiS(yplace, document.getElementById("tsumami"));
+    MoveTsumamiS(yplace, document.getElementById("tsumami"), changeind);
 }
 
 /* スマホでつまみを掴んだ直後 */
 function CatchTsumamiSS(event) {
     catchypos = event.targetTouches[0].pageY;
     tsumamiiniypos = parseInt(document.getElementById("tsumami").style.top);
-    WriteOutput(tsumamiiniypos);
 }
 
-function MoveTsumami(yplace, targ) {
+function MoveTsumami(yplace, targ, changeind) {
     yplace += (tsumamiiniypos - catchypos);
     var ypos;
     if (yplace > 275) {
@@ -76,10 +71,10 @@ function MoveTsumami(yplace, targ) {
         ypos = yplace;
     }
     targ.style.top = ypos + "px";
-    ChangeBrightness(ypos, 0);
+    ChangeBrightness(ypos, changeind);
 }
 
-function MoveTsumamiS(yplace, targ) {
+function MoveTsumamiS(yplace, targ, changeind) {
     yplace += (tsumamiiniypos - catchypos);
     var ypos;
     if (yplace > 337.5) {
@@ -92,54 +87,110 @@ function MoveTsumamiS(yplace, targ) {
         ypos = yplace;
     }
     targ.style.top = ypos + "px";
-    ChangeBrightness(ypos, 0);
+    ChangeBrightness(ypos, changeind);
 }
 
 function DeleteHamb() {
     document.getElementById("hamburger1").style.display = "none";
 }
 
-document.addEventListener('DOMContentLoaded', function () {
-    var tsumami = document.getElementById("tsumamiimg");
-    var tsumamit = document.getElementById("tsumami");
-    var close1 = document.getElementById("close1");
-    var close2 = document.getElementById("close2");
-
-    tsumami.ondragstart = CatchTsumamiSta;
-    tsumami.ondrag = CatchTsumami;
-    tsumami.ondragend = CatchTsumami;
-    tsumamit.ontouchstart = CatchTsumamiSS;
-    tsumamit.ontouchmove = CatchTsumamiS;
-    close1.onclick = CloseButton;
-    close2.onclick = CloseButton;
-
-    /* つまみの画像の大きさとPCでのつまみの当たり判定の調節 */
-    buttonimgsize = 50;
-    tsumami.style.width = buttonimgsize;
-    tsumami.style.height = buttonimgsize;
-
-    /* スマホでのつまみの当たり判定の調節 */
-    buttonsizesm = 75;
-    tsumamit.style.top = (buttonsizesm / 2 + 27.5) + "px";
-    tsumamit.style.width = buttonsizesm + "px";
-    tsumamit.style.height = buttonsizesm + "px";
-    tsumamit.style.left = "calc(50% - " + (buttonsizesm - 1) / 2 + "px)";
-    ChangeBrightness((buttonsizesm / 2 + 27.5), 0);
-}, false);
-
-
-function handleMouseMove(event) {
-    event2 = event || window.event; // IE対応
-    WriteOutput(event2.clientY);
-}
-
-function WriteOutput(str) {
-    var target = document.getElementById("output");
-    target.innerHTML = str;
-}
 
 function ChangeBrightness(ypos, changeindex) {
     var target = document.getElementsByClassName("deslig")[changeindex];
     var colornum = 255 - ypos / 2;
     target.style.backgroundColor = "rgb(" + colornum + ", " + colornum + ", " + colornum + ")";
+}
+
+/*ここからスタイルシートの初期操作*/
+
+function ApplyStyle(id, stylestr) {
+    var target = document.getElementById(id);
+    target.style = stylestr;
+}
+
+function ApplyShadowToImage(id, zind) {
+    var target = document.getElementById(id).getElementsByTagName("img");
+    var imgnum = target.length;
+    for (var i = 0; i < imgnum; i++) {
+        target[i].style = "position: relative; z-index: " + zind + ";";
+    }
+}
+
+function ApplyShadowAndStyle(id, stylestr, zind) {
+    var target = document.getElementById(id);
+    target.style = stylestr;
+    ApplyShadowToImage(id, zind);
+}
+
+function SetTsumamiStyle(id, imgsize, sizesm) {
+    var targetsm = document.getElementById(id);
+    var targetpc = targetsm.getElementsByTagName("img")[0]; 
+
+    /* つまみの画像の大きさとPCでのつまみの当たり判定の調節 */
+    targetpc.style.width = imgsize;
+    targetpc.style.height = imgsize;
+
+    /* スマホでのつまみの当たり判定の調節 */
+    targetsm.style.top = (sizesm / 2 + 27.5) + "px";
+    targetsm.style.width = sizesm + "px";
+    targetsm.style.height = sizesm + "px";
+    targetsm.style.left = "calc(50% - " + (sizesm - 1) / 2 + "px)";
+}
+
+/*ここまでスタイルシートの初期操作*/
+
+function SetNormalSlider(id, changeind) {
+    var targetsm = document.getElementById(id);
+    var targetpc = targetsm.getElementsByTagName("img")[0];
+    targetpc.ondragstart = CatchTsumamiSta;
+    targetpc.ondrag = function (event) { CatchTsumami(event, changeind); };
+    targetpc.ondragend = function (event) { CatchTsumami(event, changeind); };
+    targetsm.ontouchstart = CatchTsumamiSS;
+    targetsm.ontouchmove = function (event) { CatchTsumamiS(event, changeind); };
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+
+    /* 各種スタイルシート適用 */
+    var shadowsty = "box-shadow: 0px 0px 0px 1px rgb(0, 0, 0) inset;";
+    var close1sty = "position: absolute; height: 75px; top: 50px; left: 50%;" + shadowsty;
+    var close2sty = "position: absolute; height: 25px; top: 125px; left: 50%;" + shadowsty;
+    var hamburgersty = "position: absolute; height: 100px ;top: 50px; left: calc(50% - 100px);" + shadowsty;
+    var slidersty = "position: absolute; height: 400px ;top: 150px; left: calc(50% - 100px);" + shadowsty;
+    var tsumamisty = "position: absolute;filter: drop-shadow(0px 0px 1px rgb(0, 0, 0));";
+
+    ApplyShadowAndStyle("close1_2", close1sty, -1);
+    ApplyShadowAndStyle("close2_2", close2sty, -1);
+    ApplyShadowAndStyle("hamburger1_2", hamburgersty, -1);
+    ApplyShadowAndStyle("slider_2", slidersty, -1);
+    ApplyShadowAndStyle("tsumami_2", tsumamisty, 0);
+
+    ApplyShadowAndStyle("close1", close1sty, -1);
+    ApplyShadowAndStyle("close2", close2sty, -1);
+    ApplyShadowAndStyle("hamburger1", hamburgersty, -1);
+    ApplyShadowAndStyle("slider", slidersty, -1);
+    ApplyShadowAndStyle("tsumami", tsumamisty, 0);
+
+    var close1 = document.getElementById("close1");
+    var close2 = document.getElementById("close2");
+
+    SetNormalSlider("tsumami", 0);
+    SetNormalSlider("tsumami_2", 1);
+
+    close1.onclick = CloseButton;
+    close2.onclick = CloseButton;
+
+
+    /* つまみサイズと初期位置、当たり判定の調節*/
+    buttonimgsize = 50;
+    buttonsizesm = 75;
+    SetTsumamiStyle("tsumami", buttonimgsize, buttonsizesm);
+    SetTsumamiStyle("tsumami_2", buttonimgsize, buttonsizesm);
+
+}, false);
+
+
+function WriteOutput(str) {
+    var target = document.getElementById("output");
+    target.innerHTML = str;
 }
