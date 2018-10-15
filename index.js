01e27300dde5191b987bf70bb459d9b15dd2298c
後藤武尊 <gott@miyuki.local>
2018-10-15 14:59:44 +0900

    api server

---
+++ index.js
@@ -0,0 +1,47 @@
+const express = require("express");
+const app = express();
+var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
+var session = require('express-session');
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(session({
+    secret: 'hogehgoefugafuga',
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+        maxAge: 30 * 60 * 1000
+    }
+}));
+app.post("/api/timer", (req, res) => {
+    req.session.name = "aaa";
+    var result = {id: req.session.id, data: req.body.data};
+    res.json(result);
+    saveResult(result);
+});
+
+app.listen(3000);
+
+var saveResult = function(res) {
+    var request = require('request');
+
+    var headers = {
+        'Content-Type':'application/json'
+    };
+
+    var options = {
+        url: 'https://script.google.com/macros/s/AKfycbwYdjn8slshLn3yIAo5SAv1Qvx7ul17ws_Q_Zet5F6B2F5QnbY/exec',
+        method: 'POST',
+        headers: headers,
+        json: true,
+        form: JSON.stringify(res)
+    };
+
+    request(options, function (error, response, body) {
+        if (!error) {
+            console.log("success");
+        } else {
+            console.log(response);
+        }
+    })
+}
