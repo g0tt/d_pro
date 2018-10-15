499e50d7a31e181b7b75003693c1471de0af401a
後藤武尊 <gott@miyuki.local>
2018-10-15 13:05:39 +0900

    paging 2

--- src/modules/ListView.jsx
+++ src/modules/ListView.jsx
@@ -4,7 +4,8 @@ export default class ListView extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            items: this.props.items
+            items: this.props.items,
+            page: 0,
         }
     }
 
@@ -13,11 +14,22 @@ export default class ListView extends React.Component {
     }
 
     render() {
-        return(<div style={{width: "400px", height:"650px", backgroundColor:"#ccc", position: "relative"}}>
-                <div style={{position: "absolute", bottom: "15px", right:"20px", display: this.state.old ? "block" : "none"}}><a href="#" onClick={this.goBack.bind(this)}>戻る</a></div>
-                {this.state.items.map((item) => {return <ListItem item={item} key={item.key} listview={this} />})}
-            </div>
-        );
+        switch(this.state.page) {
+            case 2:
+                return (
+                    <div style={{width: "400px", height:"650px", backgroundColor:"#ccc", position: "relative"}}>
+                        <div style={{position: "absolute", bottom: "15px", right:"20px", display: this.state.old ? "block" : "none"}}><a href="#" onClick={this.goBack.bind(this)}>戻る</a></div>
+                        <div>hogehoge</div>
+                    </div>
+                );
+            default:
+                return(
+                    <div style={{width: "400px", height:"650px", backgroundColor:"#ccc", position: "relative"}}>
+                        <div style={{position: "absolute", bottom: "15px", right:"20px", display: this.state.old ? "block" : "none"}}><a href="#" onClick={this.goBack.bind(this)}>戻る</a></div>
+                        {this.state.items.map((item) => {return <ListItem item={item} key={item.key} listview={this} />})}
+                    </div>
+                );
+        }
     }
 }
 
@@ -29,7 +41,8 @@ class ListItem extends React.Component {
     click() {
         this.props.listview.setState({
             old: this.props.listview.state,
-            items: this.props.item.children
+            items: this.props.item.children,
+            page: this.props.item.key,
         });
     }
 
